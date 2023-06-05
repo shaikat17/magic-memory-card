244ddf45079a612597eaf46b4747a91073a9f7ef
shaikat17 <shaikatpal56@gmail.com>
2023-06-06 01:08:28 +0600

    card generates successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const cardImages = [
+  {"src": "/img/helmet-1.png"},
+  {"src": "/img/potion-1.png"},
+  {"src": "/img/ring-1.png"},
+  {"src": "/img/scroll-1.png"},
+  {"src": "/img/shield-1.png"},
+  {"src": "/img/sword-1.png"},
+]
 
+function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  const shuffleCards = () => {
+    const cards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map( card => ( {...card, id: Math.random()}))
+  }
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+    </div>
   )
 }
 
